487446e82c10e695b8da60b258e66b80a5dc5fb4
Username <albavmolinanyc@gmail.com>
2022-07-18 16:31:36 -0400

    adds getters

--- exercisetracker.py
+++ exercisetracker.py
@@ -2,30 +2,46 @@
 #or MyFitnessPal. Part of your app is that a user can log an
 #exercise session by naming the exercise, the intensity, and
 #the duration.
-#
-#Write a class called ExerciseSession. ExerciseSession
-#should have a constructor that requires two strings and an
-#integer: the strings represent the exercise name and the
-#exercise intensity, which will be "Low", "Moderate", or
-#"High". The integer will represent the length of the
-#exercise session in minutes. These should be saved in the
-#instance of the class.
-#
-#Then, add three getters to the class. The getters should
-#be named get_exercise, get_intensity, and get_duration,
-#and should return the exercise string, the exercise
-#intensity, and the duration, respectively.
-#
-#The setters should be named set_exercise, set_intensity,
-#and set_duration. Each should have one parameter (besides
-#self), which should be stored as the new value of
-#exercise, intensity, or duration. You may assume only
+
+#Write a class called ExerciseSession. 
+# ExerciseSession should have a constructor that requires two strings and an integer: the strings represent the exercise name and the
+#exercise intensity, which will be "Low", "Moderate", or "High". The integer will represent the length of the exercise session in minutes. 
+# These should be saved in the instance of the class.
+class ExerciseSession:
+    def __init__(self, exercise_name, intensity, duration):
+        self.exercise_name = exercise_name
+        self.intensity = intensity
+        self.duration = duration
+   
+    def get_exercise(self):
+        exercise_name = self.exercise_name
+        return exercise_name
+        
+
+    
+    def get_intensity(self):
+        intensity = self.intensity 
+        return intensity
+
+    
+    def get_duration(self):
+        duration = self.get_duration  
+        return duration
+        
+
+
+
+
+
+
+
+
+
+
+#The setters should be named set_exercise, set_intensity, and set_duration. Each should have one parameter (besides self), which should be stored as the new value of exercise, intensity, or duration. You may assume only
 #valid values will be passed in.
 #
-#HINT: You don't have to do any logging like you saw in
-#the video! That was just an example of one benefit of
-#using getters and setters, but this problem does not ask
-#you to do that.
+#HINT: You don't have to do any logging like you saw inthe video! That was just an example of one benefit of using getters and setters, but this problem does not ask you to do that.
 
 
 #Add your code here!
