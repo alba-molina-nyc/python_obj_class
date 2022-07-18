78699f14b10b3be1308c952fc4e0901472dbe121
Username <albavmolinanyc@gmail.com>
2022-07-18 14:56:51 -0400

    this repo is for practice

---
+++ school/student.py
@@ -0,0 +1,7 @@
+class Student:
+    def __init__(self, studentName, enrolled):
+        self.studentName = studentName
+        self.GPA = 0.0
+        self.creditHours = 0
+        self.enrolled = enrolled
+        self.classes = []
